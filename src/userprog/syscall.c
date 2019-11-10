d81d46e4e29af479a16a3809e1dcabc74edfb083
Raghav Khanna <rk4718@ic.ac.uk>
2019-11-10 16:38:21 +0100

    remove magic numbers for various headers

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -12,6 +12,21 @@
 #include "process.h"
 #define INVALID_OPEN -1
 
+#define ARG_NUM_HALT 0
+#define ARG_NUM_EXIT 1
+#define ARG_NUM_EXEC 1
+#define ARG_NUM_WAIT 1
+#define ARG_NUM_CREATE 2
+#define ARG_NUM_REMOVE 1
+#define ARG_NUM_OPEN 1
+#define ARG_NUM_FILESIZE 1
+#define ARG_NUM_READ 3
+#define ARG_NUM_WRITE 3
+#define ARG_NUM_SEEK 2
+#define ARG_NUM_TELL 1
+#define ARG_NUM_CLOSE 1
+
+
 static void syscall_handler (struct intr_frame *);
 
 static int halt(void **);
@@ -50,6 +65,7 @@ static void filesystem_access_lock() {
 static void filesystem_access_unlock() {
   lock_release(&filesystem_lock);
 }
+
 
 void
 syscall_init (void) 
